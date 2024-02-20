0de4726ad6f43e0926bbe4b5975a45a2e5e1579b
Melody Sylvestre <melody.sylvestre@Mels-MacBook-Pro.local>
2024-02-20 15:58:11 +0000

    homepage basic structure without navbar

---
+++ portfolio-website/src/app/page.tsx
@@ -0,0 +1,21 @@
+import { ContactBar } from "@/modules/ContactBar/ContactBar";
+
+export default function Home() {
+  return (
+    <>
+      <main className="flex flex-row justify-around p-12">
+        <img src="images/me_crop.jpg" className="max-w-sm" />
+        <div className="max-w-lg">
+          <h1 className="text-5xl">Melody Sylvestre</h1>
+          <h2 className="text-4xl">Full Stack Sofware Engineer</h2>
+          <p className="text-3xl">Hi and Welcome on my portfolio</p>
+        </div>
+      </main>
+      <div className="flex flex-row justify-end p-12">
+        <ContactBar />
+      </div>
+    </>
+  );
+
+}
+// TODO: change favicon.ico 
---
+++ portfolio-website/src/modules/ContactBar/ContactBar.tsx
@@ -0,0 +1,19 @@
+export const ContactBar: React.FC = () => {
+	return (
+		<div className="flex flex-row flex-wrap justify-start items-center gap-12">
+			<a href="mailto:melody.sylvestre@outlook.com"><img src="images/email_logo.png" alt="An email logo" className="h-24" /></a>
+			<a href="https://www.linkedin.com/in/melody-sylvestre/"><img src="images/linkedin_logo.png"
+				alt="LinkedIn logo" className="h-24" /></a>
+			<a href="https://github.com/melody-sylvestre"><img src="images/GitHub_logo.png" alt="GitHub logo" className="h-24" /></a>
+		</div>
+	)
+}
+
+
+
+
+{/* <footer>
+	<p>Illustration by <a
+		href="https://www.freepik.com/free-vector/hand-drawn-business-coffee-illustration_28924485.htm#query=programming%20hands&position=33&from_view=search&track=ais">Freepik</a>
+		- © Copyright 2023</p>
+</footer> */}
