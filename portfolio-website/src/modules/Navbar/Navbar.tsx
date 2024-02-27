b3bb62c5cc35fc6a43a9abf23be457ab9da42f40
Melody Sylvestre <melody.sylvestre@Mels-MacBook-Pro.local>
2024-02-27 12:53:53 +0000

    moved the homePage in modules and styled the Navbar

--- portfolio-website/src/modules/Navbar/Navbar.tsx
+++ portfolio-website/src/modules/Navbar/Navbar.tsx
@@ -1,13 +1,24 @@
+"use client"
 import Link from "next/link"
-
+import { usePathname } from "next/navigation"
 export const Navbar = (): JSX.Element => {
+    const path = usePathname()
+
 
     return (
-        <nav>
-            <Link href="/">Home</Link>
-            <Link href="/aboutMe">About Me</Link>
-            <Link href="/portfolio">Portfolio</Link>
-        </nav>
+        <>
+            <nav className="flex flex-row justify-center min-w-[80vw] sm:min-w-0 gap-8 px-0 py-12 sm:hidden">
+                {path !== "/" && <Link className="text-xl  hover:font-bold" href="/">Home</Link>}
+                {path !== "/aboutMe" && <Link className="text-xl hover:font-bold" href="/aboutMe">About me</Link>}
+                {path !== "/portfolio" && <Link className="text-xl hover:font-bold" href="/portfolio">Portfolio</Link>}
+            </nav>
+
+            <nav className="hidden sm:flex flex-row justify-center min-w-[80vw] gap-8 p-12 ">
+                <Link className="text-2xl  hover:font-bold" href="/">Home</Link>
+                <Link className="text-2xl hover:font-bold" href="/aboutMe">About me</Link>
+                <Link className="text-2xl hover:font-bold" href="/portfolio">Portfolio</Link>
+            </nav>
+        </>
     )
 
 }
