81cf26f891e34a66a3f64cf1b7a55c1e543c1f24
Melody Sylvestre <melody.sylvestre@Mels-MacBook-Pro.local>
2024-02-27 13:28:29 +0000

    styled contact bar and improved styling of homepage

--- portfolio-website/src/modules/ContactBar/ContactBar.tsx
+++ portfolio-website/src/modules/ContactBar/ContactBar.tsx
@@ -1,13 +1,13 @@
 export const ContactBar: React.FC = () => {
 	return (
-		<div className="flex flex-row justify-end p-12">
-			<div className="flex flex-row flex-wrap justify-start items-center gap-12">
-				<a href="mailto:melody.sylvestre@outlook.com"><img src="images/email_logo.png" alt="An email logo" className="h-24" /></a>
-				<a href="https://www.linkedin.com/in/melody-sylvestre/"><img src="images/linkedin_logo.png"
-					alt="LinkedIn logo" className="h-24" /></a>
-				<a href="https://github.com/melody-sylvestre"><img src="images/GitHub_logo.png" alt="GitHub logo" className="h-24" /></a>
-			</div>
+		// <div className="flex flex-row justify-end p-12">
+		<div className="flex flex-row flex-wrap gap-12 justify-center" >
+			<a href="mailto:melody.sylvestre@outlook.com"><img src="images/email_logo.png" alt="An email logo" className="h-20 w-20 object-cover" /></a>
+			<a href="https://www.linkedin.com/in/melody-sylvestre/"><img src="images/linkedin_logo.png"
+				alt="LinkedIn logo" className="h-20" /></a>
+			<a href="https://github.com/melody-sylvestre"><img src="images/GitHub_logo.png" alt="GitHub logo" className="h-20" /></a>
 		</div>
+		/* </div> */
 	)
 }
 
