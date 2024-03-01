3f5e60860fe22892d9bda51c639d97d898b5607c
Melody Sylvestre <melody.sylvestre@Mels-MacBook-Pro.local>
2024-03-01 17:25:10 +0000

    portfolio page added

--- portfolio-website/src/modules/AboutMe/AboutMe.tsx
+++ portfolio-website/src/modules/AboutMe/AboutMe.tsx
@@ -6,18 +6,17 @@ export const AboutMe = (): JSX.Element => {
     return (
         <div className="flex flex-col items-center justify-evenly py-8 px-12 bg-cover min-h-[100vh] bg-center bg-[url('../../public/images/horse_head_nebula.jpg')]">
             <Navbar />
-            <div className="flex flex-row flex-wrap min-w-[80%] justify-around items-center bg-gray-950/50 p-4 rounded-lg">
+            <div className="flex flex-row flex-wrap gap-8 min-w-[80vw] justify-around items-center bg-gray-950/50 p-4 rounded-lg">
                 <img src="images/hands_on_laptop.jpg" className="w-96 object-cover rounded-xl"
                     alt="A drawing of someone using a laptop while taking notes on a notepad." />
                 <div className="flex flex-col items-start max-w-xl justify-evenly gap-4" >
                     <h1 className="text-4xl sm:text-6xl font-serif place-self-center">About me</h1>
-                    {/* TODO: improve the text in this section - shorter more focused */}
-                    <p className="text-lg">Hi, I'm Melody! I am a full stack software engineer for Nando's UK, based in Bristol. I love crafting inventive solutions to problems with elegant code.</p>
+                    <p className="text-lg">Hi, I'm Melody! I am a full stack software engineer for <a href="https://www.nandos.co.uk" target="_blank" className="text-rose-300" >Nando's UK</a>, based in Bristol. I love crafting inventive solutions to problems with elegant code.</p>
                     <p className="text-lg">I'm particularly fond of working with Javascript and exploring the capabilities of Google Cloud Platform (GCP). That said, I'm always up for experimenting with new technologies and software to keep things interesting!</p>
 
                     <p className="text-lg">Here's a list of a few technologies  I have been working with:</p>
                     <div className="skills_and_cv_box flex flex-col items-center max-w-5xl gap-4 place-self-center">
-                        <ul className="skills_grid list-disc sm:grid sm:grid-cols-3 gap-8 text-lg justify-around">
+                        <ul className="skills_grid list-disc sm:grid sm:grid-cols-3 gap-8 text-xl justify-around">
                             <li>Node.js</li>
                             <li>Next.js</li>
                             <li>React.JS</li>
@@ -28,14 +27,15 @@ export const AboutMe = (): JSX.Element => {
                             <li>GraphQL</li>
                             <li>REST APIs</li>
                             <li>Python</li>
-                            <li>HTML</li>
-                            <li>CSS / SCSS</li>
+                            <li>PHP</li>
+                            <li>HTML / CSS </li>
                         </ul>
-                        <a className="solo_link rounded-full bg-rose-400 p-2 text-lg font-bold hover:text-cl hover:bg-rose-500" href="otherAssets/MSylvestre_CV.pdf" target="_blank">Download my CV</a>
+                        <a className="solo_link rounded-full bg-rose-400 p-2 text-lg font-bold hover:bg-rose-500" href="otherAssets/MSylvestre_CV.pdf" target="_blank">Download my CV</a>
                     </div>
                 </div>
             </div>
             <ContactBar />
+
         </div>
     )
 
--- portfolio-website/src/modules/ProjectCard/ProjectCard.tsx
+++ portfolio-website/src/modules/ProjectCard/ProjectCard.tsx
@@ -13,16 +13,16 @@ export type projectCardProps = {
 
 export const ProjectCard = (props: projectCardProps): JSX.Element => {
     return (
-        <div className="project_card" key={props.project.title}>
-            <img src={props.project.image} alt={props.project.altDescription} />
-            <h3>{props.project.title}</h3>
-            <div className="project_text">
-                <p>{props.project.description}</p>
-                <div className="project_links_bar">
-                    <a href={props.project.githubLink} target="_blank">Github</a>
-                    {props.project.liveLink && <a href={props.project.liveLink} target="_blank">Live</a>}
+        <div className="flex flex-row flex-wrap-reverse justify-start xl:justify-between gap-6 items-center xl:w-[80vw] bg-gray-950/50 rounded-lg p-8 border border-rose-300">
+            <div className="xl:max-w-[450px]">
+                <h2 className="text-2xl">{props.project.title}</h2>
+                <p className="text-md py-4">{props.project.description}</p>
+                <div className="flex flex-row flex-wrap justify-center sm:justify-start gap-12 justify-start">
+                    <a href={props.project.githubLink} target="_blank" className="rounded-full bg-rose-400 p-2 font-bold hover:bg-rose-500" >Github</a>
+                    {props.project.liveLink && <a href={props.project.liveLink} target="_blank" className="rounded-full px-[20px] bg-rose-400 p-2  font-bold hover:bg-rose-500">Live</a>}
                 </div>
             </div>
+            <img className="w-[450px] object-cover rounded-lg " src={props.project.image} alt={props.project.altDescription} />
         </div>
     )
 }
