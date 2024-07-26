14eb22bbe89b600ab08e19e44d717b727dd96431
Melody Sylvestre <melody.sylvestre@Mels-MacBook-Pro.local>
2024-07-26 16:32:16 +0100

    reorganising folder

--- portfolio-website/src/app/aboutMe/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AboutMe } from "@/modules/AboutMe/AboutMe";
-import { ContactBar } from "@/modules/ContactBar/ContactBar";
-import { Navbar } from "@/modules/Navbar/Navbar"
-
-export default function aboutMe() {
-    return (
-        <AboutMe />
-    )
-}
