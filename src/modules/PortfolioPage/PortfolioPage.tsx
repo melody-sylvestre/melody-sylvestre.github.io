70c15d886486aeb3674ae16aafab0414a43270ec
Melody Sylvestre <melody.sylvestre@nandos.co.uk>
2025-01-03 18:09:12 +0000

    minor improvements of website

--- src/modules/PortfolioPage/PortfolioPage.tsx
+++ src/modules/PortfolioPage/PortfolioPage.tsx
@@ -1,28 +1,24 @@
 import { Project, ProjectCard } from "@/modules/ProjectCard/ProjectCard";
 import projectsList from "../../modules/projects.json";
-import { Navbar } from "@/modules/Navbar/Navbar";
 import { ContactBar } from "../ContactBar/ContactBar";
 
 export const PortfolioPage = (): JSX.Element => {
   const projects: Array<Project> = projectsList.projects;
   return (
-    <div className="flex flex-col items-center gap-12 justify-evenly py-8 px-12 bg-cover min-h-[100vh] bg-center bg-[url('../../public/images/horse_head_nebula.jpg')]">
-      <Navbar />
+    <div className="flex flex-col gap-12 items-center justify-evenly">
+      <p className="w-[80vw] text-center text-lg">
+        Here&rsquo;s an overview of my personal projects, as well as the team
+        projects I worked on as a student of{" "}
+        <a
+          href="https://io-academy.uk"
+          target="_blank"
+          className="text-rose-300"
+        >
+          the iO Academy bootcamp.
+        </a>
+      </p>
 
-      <div className="flex flex-col gap-12 items-center justify-evenly">
-        {/* <h1>Portfolio</h1> */}
-        <p className="w-[80vw] text-center text-lg">
-          Here&rsquo;s an overview of my personal projects, as well as the team
-          projects I worked on as a student of{" "}
-          <a
-            href="https://io-academy.uk"
-            target="_blank"
-            className="text-rose-300"
-          >
-            the iO Academy bootcamp.
-          </a>
-        </p>
-
+      <div className="flex flex-col flex-wrap gap-12 ">
         {projects.map((project) => {
           return <ProjectCard key={project.title} project={project} />;
         })}
